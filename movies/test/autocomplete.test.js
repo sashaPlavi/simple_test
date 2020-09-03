382f00832f78105576b437d36fa522345871d5e4
SashaPlavi <sasha.bingulac@gmail.com>
2020-09-03 21:35:40 +0200

    wait for it ....

--- movies/test/autocomplete.test.js
+++ movies/test/autocomplete.test.js
@@ -1,4 +1,22 @@
-it("shows autocomplete", () => {
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 3000);
+  });
+};
+
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
@@ -18,7 +36,19 @@ it("shows autocomplete", () => {
       return movie.Title;
     },
   });
-
+});
+it("dropdown starts closed", () => {
   const dropdown = document.querySelector(".dropdown");
   expect(dropdown.className).not.to.include("is-active");
+});
+
+it("after searching, dopdown open", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
+
+  await waitFor(".dropdown-item");
+
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
 });
