d485f80f81999f958b6d08c387b878761588f07d
SashaPlavi <sasha.bingulac@gmail.com>
2020-09-15 18:40:49 +0200

    running trow files

--- tme/index.js
+++ tme/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("running test");
+const Runner = require("./runner");
+
+const runner = new Runner();
+
+const run = async () => {
+  await runner.collectFiles(process.cwd());
+  runner.runnTest();
+};
+
+run();
