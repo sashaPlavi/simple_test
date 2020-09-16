39a5fec3b33eaf88345582370e982ffaf5b78bbf
SashaPlavi <sasha.bingulac@gmail.com>
2020-09-16 18:17:46 +0200

    adding global functions

---
+++ tme/sempleproject/test/forEach.test.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+
+const { forEach } = require("../index");
+
+let numbers;
+beforeEach(() => {
+  numbers = [1, 2, 3];
+});
+
+it("shuld sum an arry", () => {
+  let total = 0;
+  forEach(numbers, (value) => {
+    total += value;
+  });
+
+  assert.strictEqual(total, 6);
+  numbers.push(3);
+  numbers.push(3);
+  numbers.push(3);
+});
+it("before each", () => {
+  assert.strictEqual(numbers.length, 3);
+});
