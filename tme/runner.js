b4c2146567473a40acf0fa34829ad82038789144
SashaPlavi <sasha.bingulac@gmail.com>
2020-09-16 19:03:43 +0200

    collors and formating

--- tme/runner.js
+++ tme/runner.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const chalk = require("chalk");
+
 class Runner {
   constructor() {
     this.testFiles = [];
@@ -8,7 +10,29 @@ class Runner {
 
   async runnTest() {
     for (let file of this.testFiles) {
-      require(file.name);
+      console.log(chalk.gray(`===runing ${file.shortName}`));
+      const beforEaches = [];
+      global.beforeEach = (fn) => {
+        beforEaches.push(fn);
+      };
+      global.it = (desc, fn) => {
+        beforEaches.forEach((func) => func());
+        try {
+          fn();
+          console.log(chalk.green(`\t OK - ${desc}`));
+        } catch (err) {
+          const message = err.message.replace(/\n/g, "\n\t\t");
+          console.log(chalk.red(`\t X -${desc}`));
+          console.log(chalk.red(`\t error mesage : ${message}`));
+        }
+      };
+
+      try {
+        require(file.name);
+      } catch (err) {
+        // console.log(`error loading file ${file.name} `);
+        console.log(chalk.red(err));
+      }
     }
   }
 
@@ -20,7 +44,7 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes(".test.js")) {
-        this.testFiles.push({ name: filepath });
+        this.testFiles.push({ name: filepath, shortName: file });
       } else if (stats.isDirectory()) {
         const childFiles = await fs.promises.readdir(filepath);
 
